45ceac6661f73f3366f544c9c79eabd2f1427c50
Andrew <andrewmatysiak@hotmail.com>
2022-10-06 19:47:01 +1100

    third commit && almost complete

--- Main.java
+++ Main.java
@@ -1,18 +1,32 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
-public class Main {
+class Main {
+    static boolean valid = true;
+
     public static void main(String[] args) {
+
         //Scanner keyboard = new Scanner(System.in);
         //String phrase = keyboard.nextLine();
-
         String test = "This snake eats straw";
-        String[] arr = test.split(" ");
-        System.out.println(Arrays.toString(arr));
+        String test2 = test.toLowerCase();
+        String[] arr = test2.split("");
 
-        for (int i = 0; i <= arr.length - 1; i++) {
-            String[] letters = arr[i].split("");
-            System.out.println(Arrays.toString(letters));
+        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i <= arr.length-1; i++){
+            int testIndex = 0;
+
+            if (arr[i].equals(" ")){
+                testIndex = i;
+                if (!arr[testIndex-1].equals(arr[testIndex+1])){
+                    valid = false;
+                }
             }
         }
-    }
+
+        if(valid) {
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Not Valid");
+        }}
+}
